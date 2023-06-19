ac0afd1a7c70fa55cbc3e9a0dbd6645971807e13
Anthony Codinach <ant@imac.lan>
2023-06-19 18:26:19 -0400

    Added svg code to shapes js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { Square, Triangle, Circle } = require("./lib/shapes");
 
 const questions = [
     { type: "input", name: "logoText", message: "What 3 characters would you like for your logo?" },
     {
         type: "input",
         name: "textColor",
-        message: "What color would you like for your logo text?"
+        message: "Please enter a color keyword (OR a hexadecimal number) for your logo text."
     },
     {
         type: "list",
@@ -21,18 +22,21 @@ const questions = [
     {
         type: "input",
         name: "shapeColor",
-        message: "What color would you like for your shape?"
-    },
-    {
-        type: "input",
-        name: "emailAddress",
-        message: "Please enter your email address"
+        message: "Please enter a color keyword (OR a hexadecimal number) for your logo shape."
     }
 ];
 
 function init() {
     inquirer.prompt(questions).then((response) => {
-        fs.writeFile(".svg", generateMarkdown(response), (error) => {
+        let newShape;
+        if (response.logoShape === "Circle") {
+            newShape = new Circle(response.shapeColor, response.textColor, response.logoText)
+        } else if (response.logoShape === "Square") {
+            newShape = new Square(response.shapeColor, response.textColor, response.logoText)
+        } else {
+            newShape = new Triangle(response.shapeColor, response.textColor, response.logoText)
+        }
+        fs.writeFile("square.svg", newShape.generate(response), (error) => {
             if (error) {
                 console.log(error);
             } else {
