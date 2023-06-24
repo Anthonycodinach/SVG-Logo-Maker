60255eddf1aae4a8adf0995f117df7cf43eb14e9
Anthony Codinach <ant@imac.lan>
2023-06-24 17:24:26 -0400

    finalized tests - all passed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Square, Triangle, Circle } = require("./lib/shapes");
 
+// Inquirer questions
 const questions = [
     { type: "input", name: "logoText", message: "What 3 characters would you like for your logo?" },
     {
@@ -25,10 +26,13 @@ const questions = [
         message: "Please enter a color keyword (OR a hexadecimal number) for your logo shape."
     }
 ];
-
+// Function to generate the new shape based on user input
 function init() {
     inquirer.prompt(questions).then((response) => {
         let newShape;
+        if (response.logoText.length > 3) {
+            console.log("You may only enter up to 3 characters for your logo text, please try again")
+        } else {
         if (response.logoShape === "Circle") {
             newShape = new Circle(response.shapeColor, response.textColor, response.logoText)
         } else if (response.logoShape === "Square") {
@@ -42,7 +46,7 @@ function init() {
             } else {
                 console.log("SVG file succesfully generated!");
             }
-        });
+        })};
     });
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,7 @@ describe('Square', () => {
     describe('new Square', () => {
       it('should dynamically create a new square', () => {
         const shape = new Square();
-        this.text.length === 3;
-        expect(shape.generate("blue","white","AMC")).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> 
+        expect(shape.generate()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> 
         <rect x="50" height="200" width="200" fill="${this.shapeColor}" />
 
         <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">
@@ -19,7 +18,12 @@ describe('Square', () => {
     describe('new Circle', () => {
       it('should dynamically create a new circle', () => {
         const shape = new Circle();
-        expect(shape.generate()).toEqual('<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="blue" />');
+        expect(shape.generate()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> 
+        <circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${this.shapeColor}" />
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">
+            ${this.text}
+        </text> </svg>`);
       });
     });
   });
@@ -28,7 +32,12 @@ describe('Square', () => {
     describe('new Triangle', () => {
       it('should dynamically create a new triangle', () => {
         const shape = new Triangle();
-        expect(shape.generate()).toEqual('<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="blue" />');
+        expect(shape.generate()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> 
+        <polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.shapeColor}" />
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">
+            ${this.text}
+        </text> </svg>`);
       });
     });
   });
