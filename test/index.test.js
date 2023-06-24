eca82f654d92e8e246ed801878e34af6c5d9dd0a
Anthony Codinach <ant@imac.lan>
2023-06-24 16:52:35 -0400

    working on tests

--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,13 @@ describe('Square', () => {
     describe('new Square', () => {
       it('should dynamically create a new square', () => {
         const shape = new Square();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual('<rect x="50" height="200" width="200" fill="blue" />');
+        this.text.length === 3;
+        expect(shape.generate("blue","white","AMC")).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> 
+        <rect x="50" height="200" width="200" fill="${this.shapeColor}" />
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">
+            ${this.text}
+        </text> </svg>`);
       });
     });
   });
@@ -14,8 +19,7 @@ describe('Square', () => {
     describe('new Circle', () => {
       it('should dynamically create a new circle', () => {
         const shape = new Circle();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual('<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="blue" />');
+        expect(shape.generate()).toEqual('<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="blue" />');
       });
     });
   });
@@ -24,8 +28,7 @@ describe('Square', () => {
     describe('new Triangle', () => {
       it('should dynamically create a new triangle', () => {
         const shape = new Triangle();
-        shape.setColor("blue");
-        expect(shape.render()).toEqual('<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="blue" />');
+        expect(shape.generate()).toEqual('<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="blue" />');
       });
     });
   });
